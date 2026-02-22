e916c617ec2d8166153443119849f769f14d0262
captainkirk854 <patrickfraioli1@hotmail.com>
2026-02-22 13:41:29 +0000

    EnsembleExecutor changed to use ISingleModelExecutor parameter

--- OpenAIApiClientKit/OpenAIApiClient/Orchestration/Execution/EnsembleExecutor.cs
+++ OpenAIApiClientKit/OpenAIApiClient/Orchestration/Execution/EnsembleExecutor.cs
@@ -8,15 +8,15 @@ namespace OpenAIApiClient.Orchestration.Execution
     using OpenAIApiClient.Interfaces.Orchestration.Execution;
     using OpenAIApiClient.Models.Chat.Request;
 
-    public sealed class EnsembleExecutor(SingleModelExecutor singleModelExecutor) : IEnsembleExecutor
+    public sealed class EnsembleExecutor(ISingleModelExecutor singleModelExecutor) : IEnsembleExecutor
     {
-        private readonly SingleModelExecutor singleModelExecutor = singleModelExecutor;
+        private readonly ISingleModelExecutor singleModelExecutor = singleModelExecutor;
 
         public async Task<IReadOnlyList<AIModelResponse>> ExecuteAsync(ClientRequestBuilder requestBuilder, IExecutionContext context, CancellationToken cancelToken)
         {
             IEnumerable<Task<AIModelResponse>> tasks = context.Models.Select(model =>
             {
-                // Override request with ensemble model ..
+                // Build the request by overriding the model in the request builder with the current model for this iteration of the loop ..
                 ChatCompletionRequest chatRequest = requestBuilder
                     .WithModel(model.Name)
                     .Build();
