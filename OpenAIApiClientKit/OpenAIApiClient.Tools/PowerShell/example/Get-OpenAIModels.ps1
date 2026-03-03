37dcd80bc7271226842df1ebccfb4478047240cf
captainkirk854 <patrickfraioli1@hotmail.com>
2026-03-03 19:17:26 +0000

    more PowerShell fun

---
+++ OpenAIApiClientKit/OpenAIApiClient.Tools/PowerShell/example/Get-OpenAIModels.ps1
@@ -0,0 +1,30 @@
+﻿function Get-OpenAIModels 
+{
+<#
+  .SYNOPSIS
+  Get a list of available OpenAI models from OpenAI API ..
+  .NOTES
+  .LINK
+ #>
+    param
+    (
+        [Parameter(Mandatory=$false)]
+        [string]$ApiKey = $env:OPENAI_API_KEY
+    )
+
+    if (-not $ApiKey) 
+    {
+        throw "No API key provided. Set OPENAI_API_KEY or pass -ApiKey."
+    }
+
+    $headers = @{
+        "Authorization" = "Bearer $ApiKey"
+    }
+
+    $response = Invoke-RestMethod `
+        -Uri "https://api.openai.com/v1/models" `
+        -Headers $headers `
+        -Method GET
+
+    return $response.data
+}
---
+++ OpenAIApiClientKit/OpenAIApiClient.Tools/PowerShell/example/OpenAI.psm1
@@ -0,0 +1,14 @@
+﻿# Export Module Functions for OpenAI API Client Tools
+
+. $PSScriptRoot/Get-OpenAIModels.ps1
+. $PSScriptRoot/Get-OpenAIModelInfo.ps1
+
+Export-ModuleMember -Function Get-OpenAIModels, Get-OpenAIModelInfo
+
+<#
+Sample Usage:
+
+    > Import-Module ./OpenAI.psm1
+    > Get-OpenAIModelInfo | Format-Table
+    > Get-OpenAIModelInfo | Format-Table Model, InputCostUSD, OutputCostUSD, CapabilitiesCSV  
+#>
